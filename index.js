271c138ce3ac34ffea70464c38b200df25aec068
Geryke18 <nagyger.91@gmail.com>
2022-02-19 22:07:58 +0100

    jsonDataBase

--- index.js
+++ index.js
@@ -16,6 +16,63 @@ app.get('/', (req, res) => {
     });
 });
 
+app.get('/getdata', (req, res) => {
+  let rawData = fs.readFileSync("DataBase.json");
+  let data = JSON.parse(rawData);
+  res.json(data);
+  console.log(data);
+  return res.end();
+});
+
+app.post('/addtodo', (req, res) => {
+  let rawData = fs.readFileSync("DataBase.json");
+  let data = JSON.parse(rawData);
+  data.todos.push(req.body);
+  let stringData = JSON.stringify(data, null, 2);
+  fs.writeFileSync("DataBase.json", stringData);
+  return res.end();
+});
+
+app.post('/deltodo', (req, res) => {
+  let rawData = fs.readFileSync("DataBase.json");
+  let data = JSON.parse(rawData);
+  for( var i = 0; i < data.todos.length; i++){ 
+    if ( data.todos[i] === req.body) {
+      data.todos.splice(i, 1); 
+    }
+  }
+  let stringData = JSON.stringify(data, null, 2);
+  fs.writeFileSync("DataBase.json", stringData);
+  return res.end();
+});
+
+app.post('/deldone', (req, res) => {
+  let rawData = fs.readFileSync("DataBase.json");
+  let data = JSON.parse(rawData);
+  for( var i = 0; i < data.dones.length; i++){ 
+    if ( data.dones[i] === req.body) {
+      data.dones.splice(i, 1); 
+    }
+  }
+  let stringData = JSON.stringify(data, null, 2);
+  fs.writeFileSync("DataBase.json", stringData);
+  return res.end();
+});
+
+app.post('/donetodo', (req, res) => {
+  let rawData = fs.readFileSync("DataBase.json");
+  let data = JSON.parse(rawData);
+  data.dones.push(req.body);
+  for( var i = 0; i < data.todos.length; i++){ 
+    if ( data.todos[i] === req.body) {
+      data.todos.splice(i, 1); 
+    }
+  }
+  let stringData = JSON.stringify(data, null, 2);
+  fs.writeFileSync("DataBase.json", stringData);
+  return res.end();
+});
+
 http.listen(3000, () => {
     console.log('listening on http://localhost:3000');
 });
